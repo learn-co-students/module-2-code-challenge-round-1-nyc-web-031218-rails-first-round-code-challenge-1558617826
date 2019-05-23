43a1c70ab5cd57030f630985e30447eddab7502c
Lisa Scott <lisarenesoctt@gmail.com>
2019-05-23 11:09:03 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,16 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine.new
+  end
+
+  def create
+    
+  end
 end
