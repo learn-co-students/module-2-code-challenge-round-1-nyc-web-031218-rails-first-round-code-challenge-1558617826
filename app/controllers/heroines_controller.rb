3963e1ffc1ce83bde292b418ae17629db93d0a9b
Arslan Ali <ali.arslan319@gmail.com>
2019-05-23 11:11:41 -0400

    arslan

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,43 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(heroine_params)
+  end
+
+  def new
+    @heroine = Heroine.find
+  end
+
+  def create
+    @heroine = Heroine.find(heroine_params)
+    @heroine.update(heroine_params)
+    if @heroine = valid?
+      heroine.save
+      redirect_to heroine_path
+    else
+      flash[:notice]
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def heroine_params
+  params.require(:heroine).permit(:name, :description:)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,29 @@
 class PowersController < ApplicationController
+
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.new
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
 end
