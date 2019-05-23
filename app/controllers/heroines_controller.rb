85d8d53c8d55babd65f59e1600ddcb214990be4f
Jessica Lin <jessicaa.lin@gmail.com>
2019-05-23 10:27:34 -0400

    done with standard

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,42 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show]
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+    @powers = Power.all
+    @heroine_powers = []
+    @powers.each do |power|
+      if power.id == @heroine.power_id
+        @heroine_powers << power
+      end
+    end
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @powers = Power.all
+      flash[:notice] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,16 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+    # @power = Power.find_by(name: params[:name])
+  end
+  
+  private
+  def find_power
+    @power = Power.find(params[:id])
   end
 end
