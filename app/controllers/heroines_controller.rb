0ce13f8464545a9af13592d5c493394e5ac25070
gavinoconnor <gedmund@gmail.com>
2019-05-23 10:13:55 -0400

    Pre-Bonus Deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,47 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    if @heroine.update(heroine_params)
+      redirect_to heroine_path(@heroine)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @heroine = Heroine.find(params[:id])
+    @heroine.destroy
+    redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,37 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def new
+    @power = Power.new
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    if @power.update(power_params)
+      redirect_to power_path(@power)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @power = Power.find(params[:id])
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description, :heroine_id)
+  end
+
+
 end
