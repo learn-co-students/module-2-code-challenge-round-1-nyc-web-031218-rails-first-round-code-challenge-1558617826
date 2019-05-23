0c07db42d79ee90cc93f00142d1fbd1a59e22312
Carly Pecora <carly@carlypecora.com>
2019-05-23 10:04:25 -0400

    Got it working.

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,44 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+  	@power = Power.find(params[:id])
+  end
+
+  def new
+  	@power = Power.new
+  end
+
+  def create
+  	@power = Power.new(power_params)
+  	if @power.valid?
+  		@power.save
+      redirect_to power_path(@power)
+  	else
+  		flash[:notice] = "Unsuccessful"
+  		redirect_to new_power_path
+  	end
+  end
+
+  def edit
+  	@power = Power.find(params[:id])
+
+  end
+
+  def update
+  	@power = Power.find(params[:id])
+  	if @power.update(power_params)
+  		redirect_to power_path(@power)
+  	else
+  		flash[:notice] = "Unsuccessful"
+  		redirect_to edit_power_path
+  	end
+  end
+
+  private
+
+  def power_params
+  	params.require(:power).permit(:name, :description)
+  end
 end
