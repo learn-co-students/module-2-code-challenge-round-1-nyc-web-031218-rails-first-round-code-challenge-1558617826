6072509024612dcb10d0bd9254e47a99c368f1ee
henryk1229 <henryk1229@gmail.com>
2019-05-23 11:07:04 -0400

    couldn't implement search

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,11 +1,20 @@
 class PowersController < ApplicationController
   def index
-    @powers = Power.all
+      if params[:store]
+      @hpowers = Power.where('power LIKE ?', "%#{params[:store]}%")
+      else
+      @powers = Power.all
+    end
   end
 
 
   def show
     @power = Power.find(params[:id])
+  end
+
+  private
+  def power_params(*args)
+    params.require(:power).permit(:name, :super_name, :power_id, :store)
   end
 
 end
