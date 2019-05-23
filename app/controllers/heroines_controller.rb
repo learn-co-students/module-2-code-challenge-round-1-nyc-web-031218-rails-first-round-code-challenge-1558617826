414fdaa49b154753c9fa52a1a6136e72909d36fa
Adam Robinson <luohaoxiang@yahoo.com>
2019-05-23 10:12:27 -0400

    first

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine = Heroine.update(helper_params)
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = Power.find(@heroine.power_id)
+  end
 end
