49535cb702a864e1b4a3d08720c7b9070864630c
Kirbyasdf <kirbyAugustus@gmail.com>
2019-05-23 10:06:46 -0400

    working on bonus

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+
+  def new
+  @heroine = Heroine.new
+
+  end
+
+  def create
+    @heroine = Heroine.new(h_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to "/heroine/#{@heroine.id}"
+    else
+      flash[:notice] = @heroine.errors.messages
+      redirect_to "/heroines/new"
+  end
+end
+
+def show
+  @heroine =  Heroine.find(params[:id])
+
+end
+
+private
+
+
+def h_params
+params.require(:heroine).permit(:name, :super_name, :power_id)
+end
+
+
 end
