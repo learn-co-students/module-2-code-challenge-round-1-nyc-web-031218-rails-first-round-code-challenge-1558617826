80c228aed00ebc3ffcc82bcddf540b18eb6b342a
Won Kim <wonsae.kim@gmail.com>
2019-05-23 09:53:04 -0400

    done up to no 7

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,45 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id]).update(heroine_params)
+    
+    redirect_to heroine_path(@heroine)
+  end
+
+  def destroy
+    Heroine.destroy(params[:id])
+
+    redirect_to heroines_path
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,45 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new(power_params)
+    if @power.valid?
+      @power.save
+
+      redirect_to power_path(@power)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id]).update(power_params)
+    
+    redirect_to power_path(@heroine)
+  end
+
+  def destroy
+    Power.destroy(params[:id])
+
+    redirect_to powers_path
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
 end
