4e9c8636fb99f431e30c5875a400f1abd281d174
Jason Gomez <jason.gomez@me.com>
2019-05-23 10:12:58 -0400

    Finish Basic Requirements
    Signed-off-by: Jason Gomez <jason.gomez@me.com>

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,40 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: %i[show edit update destroy]
+  before_action :grab_all_powers, only: %i[new create edit update]
+
   def index
     @heroines = Heroine.all
   end
+
+  def show; end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:notice] = @heroine.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def grab_all_powers
+    @powers = Power.all
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,15 @@
 class PowersController < ApplicationController
+  before_action :set_power, only: %i[show edit update destroy]
   def index
     @powers = Power.all
   end
+
+  def show; end
+
+  private
+
+  def set_power
+    @power = Power.find(params[:id])
+  end
+
 end
