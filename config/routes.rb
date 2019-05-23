adfdf3ad4cd0acd819d46ccc5eea963fc26bdec1
Jake <jakelovitz@gmail.com>
2019-05-23 09:46:30 -0400

    added links to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :heroines, only: [:index] 
+  
+  #powers
+
+  #heroines
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+
 end
