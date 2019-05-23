b2d757a701a34081f7ac532a338e8c2ac4a2a782
Jake <jakelovitz@gmail.com>
2019-05-23 11:05:13 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
-  resources :heroines, only: [:index] 
+  # resources :heroines, only: [:index] 
   
   #powers
   get '/powers/:id', to: 'powers#show', as: 'power'
 
   #heroines
+  get '/heroines', to: 'heroines#index', as: 'heroines'
+  post '/heroines', to: 'heroines#index'
   get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
   post '/heroines/new', to: 'heroines#create'
   get '/heroines/:id', to: 'heroines#show', as: 'heroine'
